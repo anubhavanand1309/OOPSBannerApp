8de90c45461c9cc774635b30fa9d205939e3ae06
anubhav1309 <rudra130906@gmail.com>
2026-02-17 19:43:03 +0530

    Coded UC1 to Print OOPS to Console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,4 +1,7 @@
-public class OOPSBannerApp{
-public static void main(String [] args){
-}
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+        System.out.println("Program executed");
+    }
 }
